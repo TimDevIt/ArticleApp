716f29cfbc3af4a7a2498d50d6b74723285b1113
TimDevIt <delootos1@mail.ru>
2025-12-09 18:34:56 +0300

    chore: configure sequelize with postgres

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const db = require("./app/models");
 
 const app = express();
 
@@ -14,6 +14,20 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
 
 
 app.get("/", (req, res) => {
