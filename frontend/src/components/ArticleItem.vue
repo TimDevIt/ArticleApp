80a8f1a810d37cbc2feceb2c41a0ba1f3cf9610e
TimDevIt <delootos1@mail.ru>
2025-12-19 20:55:20 +0300

    feat: add article list components

---
+++ frontend/src/components/ArticleItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-list-item 
+      :key="article.id"
+    >
+      <div class="pa-2">
+        <div class="text-h6 mb-1">{{ article.title }}</div>
+        <div class="text-body-2 grey lighten-1">
+            {{ article.text }}
+        </div>
+      </div>
+      <template #append>
+        <v-btn
+          @click="$router.push(`/article/${article.id}`)"
+        >
+            Открыть
+        </v-btn>
+      </template>
+    </v-list-item>
+</template>
+
+<script>
+    export default {
+        props: {
+           article: {
+            type: Object, 
+            required: true
+           }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
